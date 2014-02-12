6f73046ea2faf1d315deeb806c92d4ed9f929fe2
akhilnambiar <akhilnambiar@berkeley.edu>
2014-02-12 12:21:04 -0800

    recommit

--- web.js
+++ web.js
@@ -28,20 +28,7 @@ app.listen(port, function() {
 });
 */
 
- var pg = require('pg').native
-  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
-  , client
-  , query;
 
-client = new pg.Client(connectionString);
-client.connect();
-query = client.query('CREATE TABLE visits (date date)');
-query.on('end', function() { client.end(); });
-
-client = new pg.Client(connectionString);
-client.connect();
-query = client.query('SELECT * FROM mytable');
-query.on('end', function() { client.end(); });
 var express = require('express')
   , app = express.createServer(express.logger())
   , pg = require('pg').native
