913f9279f374f700c7dc1a198aa967ad16cd1110
akhilnambiar <akhilnambiar@berkeley.edu>
2014-02-12 12:15:57 -0800

    changed the file

--- web.js
+++ web.js
@@ -1,7 +1,19 @@
 // web.js
+/*
+THIS IS THE FIRST HELLO WORLD
+
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
 
 app.use(logfmt.requestLogger());
 
@@ -13,4 +25,51 @@ app.get('/', function(req, res) {
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+*/
+
+ var pg = require('pg').native
+  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
+  , client
+  , query;
+
+client = new pg.Client(connectionString);
+client.connect();
+query = client.query('CREATE TABLE visits (date date)');
+query.on('end', function() { client.end(); });
+
+client = new pg.Client(connectionString);
+client.connect();
+query = client.query('SELECT * FROM mytable');
+query.on('end', function() { client.end(); });
+var express = require('express')
+  , app = express.createServer(express.logger())
+  , pg = require('pg').native
+  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
+  , start = new Date()
+  , port = process.env.PORT || 3000
+  , client;
+
+  client new pg.Client(connectionString);
+  client.connect();
+
+app.get('/', function(req, res) {
+  var date = new Date();
+
+  client.query('INSERT INTO visits(date) VALUES($1)', [date]);
+
+  query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
+  query.on('row', function(result) {
+    console.log(result);
+
+    if (!result) {
+      return res.send('No data found');
+    } else {
+      res.send('Visits today: ' + result.count);
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log('Listening on:', port);
 });
