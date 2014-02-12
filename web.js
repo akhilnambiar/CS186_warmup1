4e99203c8b74e1070565c725b24d81967072c31a
akhilnambiar <akhilnambiar@berkeley.edu>
2014-02-12 13:28:57 -0800

    fixed db

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express();
 var pg = require('pg');
 
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM your_table', function(err, result) {
+  client.query('SELECT * FROM login_info', function(err, result) {
     done();
     if(err) return console.error(err);
     console.log(result.rows);
