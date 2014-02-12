bc516455a57362a19e15f0b798029fb7700f03e4
akhilnambiar <akhilnambiar@berkeley.edu>
2014-02-12 14:50:10 -0800

    tried adding

--- web.js
+++ web.js
@@ -27,8 +27,8 @@ app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   //res.write('Goodbye World!');
-  res.send(users);
-  res.send('How fancy can we get with this?');
+  res.send("%s",users);
+  //res.send('How fancy can we get with this?');
 });
 
 var port = Number(process.env.PORT || 5000);
