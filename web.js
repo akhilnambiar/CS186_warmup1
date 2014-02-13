9f88f15b9380f2477751152685391d4478f6badd
akhilnambiar <akhilnambiar@berkeley.edu>
2014-02-13 15:00:12 -0800

    fixed db

--- web.js
+++ web.js
@@ -149,7 +149,7 @@ app.get('/', function(req, res) {
   //res.send('How fancy can we get with this?');
   res.writeHead(200);
   res.write('<html><body>'+body+'<br>')
-  res.end('<form action="signup" method="post">Username <input type="text" name="username"><br>Password <input type="text" name="password"><input type="submit" value="Login" onclick=this.form.action="signup"><input type="submit" value="add" onclick=this.form.action="add"></form></body></html>');
+  res.end('<form action="signup" method="post">Username <input type="text" name="username"><br>Password <input type="text" name="password"><input type="submit" value="Login" onclick=this.form.action="users/login"><input type="submit" value="add" onclick=this.form.action="users/add"></form></body></html>');
   /*
   req.on('data',function(chunk) {
     body+= chunk;
@@ -167,7 +167,7 @@ app.configure(function(){
 });
 
 
-app.post('/signup', function(req, res) {
+app.post('/users/login', function(req, res) {
     //console.log(req.body);
     res.write("<html><body>")
     var body = "<button onclick='window.location.assign(\"http://radiant-temple-1017.herokuapp.com/\");'>Click me</button>";
@@ -237,7 +237,7 @@ app.post('/signup', function(req, res) {
     */
 });
 
-app.post('/add', function(req, res) {
+app.post('/users/add', function(req, res) {
     //console.log(req.body);
     res.write("<html><body>")
     var body = "<button onclick='window.location.assign(\"http://radiant-temple-1017.herokuapp.com/\");'>Click me</button>";
